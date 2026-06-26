4c35e4911eb4041184ea6bbcea2e5d867dc3e375
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-06-26 08:06:33 +0530

    add: category model

---
+++ src/models/category.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+categorySchema.pre("save", async function () {
+  if (this.isModified("name")) {
+    this.slug = this.name
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-|-$)+/g, "");
+  }
+});
+
+export const Category = mongoose.model("Category", categorySchema);
