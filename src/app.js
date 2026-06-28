eb7f9b89214615a3d0486e6c0544af15cac9ba0f
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-06-28 18:26:42 +0530

    add: order route in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.routes.js";
 import categoryRouter from "./routes/category.routes.js";
 import productRouter from "./routes/product.routes.js";
 import cartRouter from "./routes/cart.routes.js";
+import orderRouter from "./routes/order.routes.js";
 
 const app = express();
 
@@ -35,5 +36,6 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/orders", orderRouter);
 
 export default app;
