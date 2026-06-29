9e0a68040019d48b5203d57499a999626d77b16b
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-06-29 23:35:20 +0530

    add: cart validator to routes

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -7,17 +7,26 @@ import {
   clearCart,
 } from "../controllers/cart.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import {
+  addToCartSchema,
+  updateCartItemQuantitySchema,
+} from "../validators/cart.validator.js";
 
 const router = Router();
 
 // Apply verifyJWT middleware globally to all routes below
 router.use(verifyJWT);
 
-router.route("/").post(addToCart).get(getCart).delete(clearCart);
+router
+  .route("/")
+  .post(validate(addToCartSchema), addToCart)
+  .get(getCart)
+  .delete(clearCart);
 
 router
   .route("/:productId")
-  .patch(updateCartItemQuantity)
+  .patch(validate(updateCartItemQuantitySchema), updateCartItemQuantity)
   .delete(removeCartItem);
 
 export default router;
