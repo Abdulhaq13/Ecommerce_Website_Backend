9252a8365d07f95956f28a7eab16642a70468432
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-06-29 23:08:59 +0530

    add: validators in routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -10,18 +10,35 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { isAdmin } from "../middlewares/role.middleware.js";
 import { optionalAuth } from "../middlewares/optionalAuth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../validators/product.validator.js";
 
 const router = Router();
 
 router
   .route("/")
   .get(getAllProducts)
-  .post(verifyJWT, isAdmin, upload.array("images", 5), createProduct);
+  .post(
+    verifyJWT,
+    isAdmin,
+    upload.array("images", 5),
+    validate(createProductSchema),
+    createProduct,
+  );
 
 router
   .route("/:id")
   .get(optionalAuth, getProductById)
-  .patch(verifyJWT, isAdmin, upload.array("images", 5), updateProduct)
+  .patch(
+    verifyJWT,
+    isAdmin,
+    upload.array("images", 5),
+    validate(updateProductSchema),
+    updateProduct,
+  )
   .delete(verifyJWT, isAdmin, deleteProduct);
 
 export default router;
