9b6dd1de2a56c5dfbc8903d0392d4578200df513
Syed Abdul Haq Shuttari <syedabdulhaqshuttari@gmail.com>
2026-07-02 23:05:37 +0530

    fix: update category function  -validation of name.trim forgot parenthesis  -and inside that validation only other logic was wrapped up so need to take it out of if block

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -43,29 +43,28 @@ const updateCategory = asyncHandler(async (req, res) => {
   }
 
   if (name !== undefined) {
-    if (name.trim === "") {
+    if (name.trim() === "") {
       throw new ApiError(404, "Category name cannot be empty");
     }
-
-    const existingCategory = await Category.findOne({
-      name: name.trim(),
-      _id: { $ne: id }, //"Not Equal": ignore the one I am currently editing right now
-    });
-
-    if (existingCategory) {
-      throw new ApiError(409, "Category with this name already exists");
-    }
-    category.name = name.trim();
-
-    if (isActive !== undefined) {
-      category.isActive = isActive;
-    }
-
-    await category.save();
-    return res
-      .status(200)
-      .json(new ApiResponse(200, category, "Category updated successfully"));
   }
+  const existingCategory = await Category.findOne({
+    name: name.trim(),
+    _id: { $ne: id }, //"Not Equal": ignore the one I am currently editing right now
+  });
+
+  if (existingCategory) {
+    throw new ApiError(409, "Category with this name already exists");
+  }
+  category.name = name.trim();
+
+  if (isActive !== undefined) {
+    category.isActive = isActive;
+  }
+
+  await category.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, category, "Category updated successfully"));
 });
 
 const deleteCategory = asyncHandler(async (req, res) => {
